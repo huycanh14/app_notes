be8dd8e3f69334c2d23f34c68a00368e8c51f362
Canh Huy <huycanh14@gmail.com>
2021-02-02 22:08:52 +0700

    edit css menu

--- js/index.js
+++ js/index.js
@@ -4,12 +4,14 @@ var btnDropMenu = $("#button--drop-menu"),
 	menuToggle = $("#menu-toggle"),
 	content = $("#content"),
 	backgroundGray = $(".background--gray").first(),
-	intention = $(".intention").first();
+	intention = $(".intention").first(),
+	actionList = $(".list-action");
 
 // declare constant
 
 const C_SHOW_BACK_GROUND = "show-background--gray";
 const RETURN_RIGHT = "return-right";
+// const OFFSET_RIGHT = "offset-2 content--transform";
 const OFFSET_RIGHT = "offset-2 content--transform";
 const TRANSFROM_INTENTION = "intention--transform";
 
@@ -17,17 +19,34 @@ const TRANSFROM_INTENTION = "intention--transform";
 
 $(function () {
 	btnDropMenu.click(function(event) {
-		backgroundGray.addClass(C_SHOW_BACK_GROUND);
-		menuToggle.addClass(RETURN_RIGHT);
-		content.addClass(OFFSET_RIGHT);
-		intention.addClass(TRANSFROM_INTENTION);
+		displayMenu();
 	});
 
 	backgroundGray.click(function(event) {
-		backgroundGray.removeClass(C_SHOW_BACK_GROUND);
-		menuToggle.removeClass(RETURN_RIGHT);
-		content.removeClass(OFFSET_RIGHT);
-		intention.removeClass(TRANSFROM_INTENTION);
-		
+		notDisplayMeny();
+	});
+
+	actionList.click(function(event) {
 	});
 })
+
+function actionChangeColor(argument) {
+	$("#menu-toggle>ul>li").removeClass('active').filter(argument).toggleClass('active');
+	notDisplayMeny();
+}
+
+const displayMenu  = () =>  {
+	backgroundGray.addClass(C_SHOW_BACK_GROUND);
+	menuToggle.addClass(RETURN_RIGHT);
+	content.addClass(OFFSET_RIGHT);
+	intention.addClass(TRANSFROM_INTENTION);
+	btnDropMenu.addClass('d-none');
+}
+
+const notDisplayMeny = () => {
+	backgroundGray.removeClass(C_SHOW_BACK_GROUND);
+	menuToggle.removeClass(RETURN_RIGHT);
+	content.removeClass(OFFSET_RIGHT);
+	intention.removeClass(TRANSFROM_INTENTION);
+	btnDropMenu.removeClass('d-none');
+}
